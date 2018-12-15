5d72da0f823a84e2eb7e6a30410f060c9e021e96
Garett Arrowood <garettarrowood@gmail.com>
2018-12-15 08:14:52 -0500

    Schema timestamp fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117223101) do
+ActiveRecord::Schema.define(version: 2015_11_17_223101) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
